55f454453dc3a1cce5ed2dbc2959b9e213389aa9
Charbel178 <charbelrizk7171@gmail.com>
2025-02-02 21:07:07 +0200

    adding all models

---
+++ src/models/MemberModel.js
@@ -0,0 +1,31 @@
+const db = require("./db");
+
+const Member = {
+  create: (member, callback) => {
+    const sql =
+      "INSERT INTO member (name, email, password, phoneNumber) VALUES (?, ?, ?, ?)";
+    db.query(
+      sql,
+      [member.name, member.email, member.password, member.phoneNumber],
+      callback
+    );
+  },
+
+  getAll: (callback) => {
+    db.query("SELECT * FROM member", callback);
+  },
+
+  getById: (id, callback) => {
+    db.query("SELECT * FROM member WHERE member-id = ?", [id], callback);
+  },
+
+  getByEmail: (email, callback) => {
+    db.query("SELECT * FROM member WHERE email = ?", [email], callback);
+  },
+
+  delete: (id, callback) => {
+    db.query("DELETE FROM member WHERE member-id = ?", [id], callback);
+  },
+};
+
+module.exports = Member;
---
+++ src/models/Member_gymclassModel.js
@@ -0,0 +1,28 @@
+const db = require("./db");
+
+const MemberGymClass = {
+  create: (memberGymClass, callback) => {
+    const sql = `
+      INSERT INTO member_gymclass (member-id, class_code, discount) 
+      VALUES (?, ?, ?)`;
+    db.query(
+      sql,
+      [
+        memberGymClass.member - id,
+        memberGymClass.class_code,
+        memberGymClass.discount,
+      ],
+      callback
+    );
+  },
+
+  delete: (member_id, callback) => {
+    db.query(
+      "DELETE FROM member_gymclass WHERE member-id = ?",
+      [member_id],
+      callback
+    );
+  },
+};
+
+module.exports = MemberGymClass;
