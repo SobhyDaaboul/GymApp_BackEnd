c842846d17b5a3c94af2f3697b9e05ca94a3ed0d
sobhy <Admin@SobhyDaaboul>
2025-02-06 15:29:00 +0200

    add

--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -1,22 +1,29 @@
 const Class = require("../models/ClassModel");
+const Member = require("../models/MemberModel");
 
 const ClassController = {
-  createClass: (req, res) => {
-    const classData = req.body;
-    Class.create(classData, (err, result) => {
-      if (err) return res.status(500).json(err);
-      res
-        .status(201)
-        .json({ message: "Class created successfully", id: result.insertId });
-    });
-  },
+  createClass: [
+    isLoggedIn,
+    (req, res) => {
+      const classData = req.body;
+      Class.create(classData, (err, result) => {
+        if (err) return res.status(500).json(err);
+        res
+          .status(201)
+          .json({ message: "Class created successfully", id: result.insertId });
+      });
+    },
+  ],
 
-  getAllClasses: (req, res) => {
-    Class.getAll((err, results) => {
-      if (err) return res.status(500).json(err);
-      res.json(results);
-    });
-  },
+  getAllClasses: [
+    isLoggedIn,
+    (req, res) => {
+      Class.getAll((err, results) => {
+        if (err) return res.status(500).json(err);
+        res.json(results);
+      });
+    },
+  ],
 };
 
 module.exports = ClassController;
