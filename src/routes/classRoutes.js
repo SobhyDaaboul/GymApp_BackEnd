92ccde4757a3837d5e763a50439edea761d0517f
Charbel178 <charbelrizk7171@gmail.com>
2025-02-06 10:48:33 +0200

    Done backend

--- src/routes/classRoutes.js
+++ src/routes/classRoutes.js
@@ -1,5 +1,7 @@
-const router = require("express").Router;
+const express = require("express");
 const classController = require("../controllers/ClassController");
+
+const router = express.Router();
 
 router.get("/class", classController.getAllClasses);
 
