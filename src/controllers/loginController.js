9843d3dfff1ab3b8690dbe4645ae2c95b72cd853
sobhy <Admin@SobhyDaaboul>
2025-02-15 21:37:45 +0200

    lofin works

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -37,6 +37,8 @@ class LoginController {
 
           // ✅ Send only the success message
           res.json({ message: "Login successful" });
+          // After successful login
+          localStorage.setItem("userEmail", email);
         });
       });
     } catch (error) {
