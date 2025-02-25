41ec8a7b21d57509012c766022d77ae989aebcd5
sobhy <Admin@SobhyDaaboul>
2025-02-25 18:56:01 +0200

    change name to member_id in table

--- src/models/Member_gymclassModel.js
+++ src/models/Member_gymclassModel.js
@@ -3,19 +3,19 @@ const db = require("../config/db");
 const MemberGymClass = {
   createBooking: (memberId, classCode, callback) => {
     const sql =
-      "INSERT INTO member_gymclass (membergymclass_id, class_code) VALUES (?, ?)";
+      "INSERT INTO member_gymclass (member_id, class_code) VALUES (?, ?)";
     db.query(sql, [memberId, classCode], (err, result) => {
       if (err) {
         console.error("Error inserting booking:", err);
         return callback(err, null);
       }
-      callback(null, result);
+      callback(null, result); //new commit added
     });
-  },
+  }, //new
 
   findExistingBooking: (memberId, classCode, callback) => {
     const sql =
-      "SELECT * FROM member_gymclass WHERE membergymclass_id = ? AND class_code = ?";
+      "SELECT * FROM member_gymclass WHERE member_id = ? AND class_code = ?";
     db.query(sql, [memberId, classCode], (err, rows) => {
       if (err) {
         console.error("Error finding booking:", err);
